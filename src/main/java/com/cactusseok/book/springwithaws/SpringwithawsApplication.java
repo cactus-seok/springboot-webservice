7414825131f1155ab396f0f48c6660331f9cd5ad
cactus-seok <sangpil95@gmail.com>
2021-07-20 20:52:23 +0900

    Initial commit

---
+++ src/main/java/com/cactusseok/book/springwithaws/SpringwithawsApplication.java
@@ -0,0 +1,13 @@
+package com.cactusseok.book.springwithaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwithawsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringwithawsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cactusseok/book/springwithaws/SpringwithawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cactusseok.book.springwithaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwithawsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
