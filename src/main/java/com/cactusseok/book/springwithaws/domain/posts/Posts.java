13431dda99eb03459aa528150b3d4dbd79c73ee8
cactus-seok <sangpil95@gmail.com>
2021-07-23 17:37:47 +0900

    feat: Posts 도메인모델 생성

---
+++ src/main/java/com/cactusseok/book/springwithaws/domain/posts/Posts.java
@@ -0,0 +1,40 @@
+package com.cactusseok.book.springwithaws.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter // 해당 클래스의 빌더 패턴 클래스를 생성
+@NoArgsConstructor // 기본 생성자 자동 추가
+@Entity // 테이블과 링크될 클래스임을 나타냅니다.
+public class Posts {
+
+    @Id // 해당 테이블의 PK필드를 나타냅니다.
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder // 해당 클래스의 빌더 패턴 클래스를 생성, 생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
+
+/*
+위의 Posts클래스는 한가지 특이점이 있습니다. 바로 Setter 메소드가 없다는 점입니다.
+자바빈 규약을 생각하면서 getter/setter를 무작정 생성하는 경우가 있습니다. 이렇게 되면 해당 클래스의
+인스턴스 값들이 언제 어디서 변해야 하는지 코드상으로 명확하게 구분할 수가 없어, 차후 기능 변경 시 정말 복잡해집니다.
+그래서 Entity 클래스에서는 절대 Setter 메소드를 만들지 않습니다. 대신, 해당 필드의 값 변경이 필요하면 명확히
+그 목적과 의도를 나타낼 수 있는 메소드를 추가해야만 합니다.
+ */
