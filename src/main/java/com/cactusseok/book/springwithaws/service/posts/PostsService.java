2ab2467917166ed60b8394595da5daf0642282c3
cactus-seok <sangpil95@gmail.com>
2021-07-23 17:47:35 +0900

    feat: PostsService 생성

---
+++ src/main/java/com/cactusseok/book/springwithaws/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.cactusseok.book.springwithaws.service.posts;
+
+import com.cactusseok.book.springwithaws.domain.posts.PostsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
