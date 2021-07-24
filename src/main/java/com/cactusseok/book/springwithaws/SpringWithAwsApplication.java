351a0d723d0a15c287484df56dbe0cb8a724d993
cactus-seok <sangpil95@gmail.com>
2021-07-24 22:35:12 +0900

    fix: H2 DB연결 오류 수정

--- src/main/java/com/cactusseok/book/springwithaws/SpringWithAwsApplication.java
+++ src/main/java/com/cactusseok/book/springwithaws/SpringWithAwsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringwithawsApplication {
+public class SpringWithAwsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringwithawsApplication.class, args);
+        SpringApplication.run(SpringWithAwsApplication.class, args);
     }
 
 }
