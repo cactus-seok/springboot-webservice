0f46475f4b503bc9eaa97aa3d171e59e9ce88ba7
cactus-seok <sangpil95@gmail.com>
2021-09-18 14:11:01 +0900

    feat: @LoginUser 로 코드 개선

--- src/main/java/com/cactusseok/book/springwithaws/web/IndexController.java
+++ src/main/java/com/cactusseok/book/springwithaws/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.cactusseok.book.springwithaws.web;
 
+import com.cactusseok.book.springwithaws.config.auth.LoginUser;
 import com.cactusseok.book.springwithaws.config.auth.dto.SessionUser;
 import com.cactusseok.book.springwithaws.service.posts.PostsService;
 import com.cactusseok.book.springwithaws.web.dto.PostsResponseDto;
@@ -16,12 +17,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
